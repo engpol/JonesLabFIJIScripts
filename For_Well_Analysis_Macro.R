a16a69f7a3c7f4168f960bf91d1ce152907bcba3
engpol <137185112+engpol@users.noreply.github.com>
2024-07-22 17:00:00 +0100

    Adding an output csv which does not average
    As title

--- For_Well_Analysis_Macro.R
+++ For_Well_Analysis_Macro.R
@@ -46,8 +46,37 @@ c_csv_extracted_data <- function() {
   
 } #Function to loop and append through all "Well_Number_.csv" files in parent directory, add well labels, AND  and take averages for each slice/well
 
+c_csv_extracted_data_no_average <- function() {
+  
+  list_of_extracted_csv <- list.files(path = exfolder_ext, pattern ="Well_Number_\\d+.csv", full.names = TRUE, recursive = TRUE) #Fetch all "Results_Conc.csv" files from the parent directory of all experiments
+  
+  combined_data <- read.csv(list_of_extracted_csv[1]) #Read first csv file in empty array to save making empty dataframe of right dimensions
+  
+  csv_files_no_first <- list_of_extracted_csv[2:length(list_of_extracted_csv)] ## JUST SO WE DONT ADD THE FIRST CSV IN THE LIST TWICE
+  
+  for (file in csv_files_no_first) { ##AS ABOVE BUT LOOPING THROUGH ALL REMAINING CSVS
+    temp_data <- read.csv(file)
+    combined_data <- rbind(combined_data, temp_data)
+  }
+  
+  combined_data <- transform(combined_data, Well_number = sapply(regmatches(Label, regexec("[a-zA-Z]\\d+-Site", Label)), "[", 1))
+  
+  combined_data <- transform(combined_data, Well_number = sapply(regmatches(Well_number, regexec("[a-zA-Z]\\d+", Well_number)), "[", 1))
+  
+  unique_data <- combined_data[!duplicated(combined_data$Label), ]
+  
+  distinct_data <- subset(unique_data, select = c('Well_number', 'Slice','Mean'))  
+  
+  return(distinct_data)
+  
+} #Function to loop and append through all "Well_Number_.csv" files in parent directory, add well labels, AND  and take averages for each slice/well
+
 my_data <- c_csv_extracted_data()
 
+my_data_no_average <- c_csv_extracted_data_no_average()
+
 write.csv(my_data , paste(exfolder, "/Results_Conc.csv", sep = ""), row.names = FALSE)
+
+write.csv(my_data , paste(exfolder, "/Results_Conc_No_Average.csv", sep = ""), row.names = FALSE)
 
 
